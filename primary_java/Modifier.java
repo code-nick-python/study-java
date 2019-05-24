af0b931a4d972d2b26d26a65244d24131435e558
code-nick-python <2330458484@qq.com>
2019-05-24 18:05:27 +0800

    Create Modifier.java

---
+++ primary_java/Modifier.java
@@ -0,0 +1,9 @@
+public class Modifier{
+	private String password = "abcdefg";
+	public String getPassword() {
+		return this.password;
+	}
+	public void setPassword(String new_password) {
+		this.password = new_password;
+	}
+}
