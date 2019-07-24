4afdab2ad35e14bca80b382e5376105c8c84300a
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-07-24 11:21:42 +0800

    Add Spring HelloWorld example

---
+++ HelloSpring/src/tutorial/Main.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
